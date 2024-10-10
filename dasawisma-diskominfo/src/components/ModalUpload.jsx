685980a8180ae50b752062576592246f3962d6c2
rahma aulia <qwertyrahmaaulia@gmail.com>
2024-10-10 08:20:36 +0700

    8feat: useCase, classDiagram, revisi fe admin

--- dasawisma-diskominfo/src/components/ModalUpload.jsx
+++ dasawisma-diskominfo/src/components/ModalUpload.jsx
@@ -2,14 +2,14 @@ import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
-export default function ModalUpload() {
+export default function ModalUpload({ setModalOpen }) {
   const [selectedDocs, setSelectedDocs] = useState([]);
-  const [modalOpen, setModalOpen] = useState(true);
   return (
     <>
       {/* component */}
-      <h1>{modalOpen? "true" : "false"}</h1>
-      <div className="bg-gray-500 h-screen w-screen sm:px-8 md:px-16 sm:py-8">
+      {/* <h1>{modalOpen? "true" : "false"}</h1> */}
+
+      <div className="bg-white h-screen w-screen sm:px-8 md:px-16 sm:py-8">
         <main className="container mx-auto max-w-screen-lg h-full">
           {/* file upload modal */}
           <article
@@ -21,73 +21,81 @@ export default function ModalUpload() {
             ondragenter="dragEnterHandler(event);"
           >
             {/* overlay */}
-            <div
-              id="overlay"
-              className="w-full h-full absolute top-0 left-0 pointer-events-none z-50 flex flex-col items-center justify-center rounded-md"
-            >
-              <i>
-                <svg
-                  className="fill-current w-12 h-12 mb-3 text-blue-700"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width={24}
-                  height={24}
-                  viewBox="0 0 24 24"
+            {selectedDocs.length === 0 ? (
+              <div>
+                <div
+                  id="overlay"
+                  className="w-full h-full absolute top-0 left-0 pointer-events-none z-50 flex flex-col items-center justify-center rounded-md"
                 >
-                  <path d="M19.479 10.092c-.212-3.951-3.473-7.092-7.479-7.092-4.005 0-7.267 3.141-7.479 7.092-2.57.463-4.521 2.706-4.521 5.408 0 3.037 2.463 5.5 5.5 5.5h13c3.037 0 5.5-2.463 5.5-5.5 0-2.702-1.951-4.945-4.521-5.408zm-7.479-1.092l4 4h-3v4h-2v-4h-3l4-4z" />
-                </svg>
-              </i>
-              <p className="text-lg text-blue-700">Drop files to upload</p>
-            </div>
+                  <i>
+                    <svg
+                      className="fill-current w-12 h-12 mb-3 text-blue-700"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width={24}
+                      height={24}
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M19.479 10.092c-.212-3.951-3.473-7.092-7.479-7.092-4.005 0-7.267 3.141-7.479 7.092-2.57.463-4.521 2.706-4.521 5.408 0 3.037 2.463 5.5 5.5 5.5h13c3.037 0 5.5-2.463 5.5-5.5 0-2.702-1.951-4.945-4.521-5.408zm-7.479-1.092l4 4h-3v4h-2v-4h-3l4-4z" />
+                    </svg>
+                  </i>
+                  <p className="text-lg text-blue-700">Drop files to upload</p>
+                </div>
+                <section className="h-full overflow-auto p-8 w-full h-full flex flex-col">
+                  <header className="border-dashed border-2 border-gray-400 py-12 flex flex-col justify-center items-center">
+                    <p className="mb-3 font-semibold text-gray-900 flex flex-wrap justify-center">
+                      <span>Drag and drop your</span>&nbsp;
+                      <span>files anywhere or</span>
+                    </p>
+                    <input
+                      className="mt-2 rounded-md px-3 py-1 bg-gray-200 hover:bg-gray-300 focus:shadow-outline focus:outline-none"
+                      type="file"
+                      accept=".pdf"
+                      multiple
+                      onChange={(el) =>
+                        el.target.files?.length &&
+                        setSelectedDocs(Array.from(el.target.files))
+                      }
+                    />
+                    {/* <button
+      id="button"
+      className="mt-2 rounded-sm px-3 py-1 bg-gray-200 hover:bg-gray-300 focus:shadow-outline focus:outline-none"
+    >
+      Upload a file
+    </button> */}
+                  </header>
+                </section>
+              </div>
+            ) : (
+              <section className="h-full overflow-auto p-8 w-full h-full flex flex-col">
+                <ul id="gallery" className="flex flex-1 flex-wrap -m-1">
+                  <li
+                    id="empty"
+                    className="h-full w-full text-center flex flex-col items-center justify-center items-center"
+                  >
+                    <div className="max-w-md w-full">
+                      <DocViewer
+                        documents={selectedDocs.map((file) => ({
+                          uri: window.URL.createObjectURL(file),
+                          fileName: file.name,
+                        }))}
+                        pluginRenderers={DocViewerRenderers}
+                      />
+                      <span className="text-small text-gray-500">
+                        {selectedDocs.length === 0 ? "No files selected" : ""}
+                      </span>
+                    </div>
+                  </li>
+                </ul>
+              </section>
+            )}
+
             {/* scroll area */}
-            <section className="h-full overflow-auto p-8 w-full h-full flex flex-col">
-              <header className="border-dashed border-2 border-gray-400 py-12 flex flex-col justify-center items-center">
-                <p className="mb-3 font-semibold text-gray-900 flex flex-wrap justify-center">
-                  <span>Drag and drop your</span>&nbsp;
-                  <span>files anywhere or</span>
-                </p>
-                <input
-                  className="mt-2 rounded-sm px-3 py-1 bg-gray-200 hover:bg-gray-300 focus:shadow-outline focus:outline-none"
-                  type="file"
-                  accept=".pdf"
-                  multiple
-                  onChange={(el) =>
-                    el.target.files?.length &&
-                    setSelectedDocs(Array.from(el.target.files))
-                  }
-                />
-                {/* <button
-                  id="button"
-                  className="mt-2 rounded-sm px-3 py-1 bg-gray-200 hover:bg-gray-300 focus:shadow-outline focus:outline-none"
-                >
-                  Upload a file
-                </button> */}
-              </header>
-              <h1 className="pt-8 pb-3 font-semibold sm:text-lg text-gray-900">
-                To Upload
-              </h1>
-              <ul id="gallery" className="flex flex-1 flex-wrap -m-1">
-                <li
-                  id="empty"
-                  className="h-full w-full text-center flex flex-col items-center justify-center items-center"
-                >
-                  <DocViewer
-                    documents={selectedDocs.map((file) => ({
-                      uri: window.URL.createObjectURL(file),
-                      fileName: file.name,
-                    }))}
-                    pluginRenderers={DocViewerRenderers}
-                  />
-                  <span className="text-small text-gray-500">
-                    {selectedDocs.length === 0 ? "No files selected" : ""}
-                  </span>
-                </li>
-              </ul>
-            </section>
+
             {/* sticky footer */}
             <footer className="flex justify-end px-8 pb-8 pt-4">
               <button
                 id="submit"
-                className="rounded-sm px-3 py-1 bg-blue-700 hover:bg-blue-500 text-white focus:shadow-outline focus:outline-none"
+                className="rounded-md px-3 py-1 bg-blue-700 hover:bg-blue-500 text-white focus:shadow-outline focus:outline-none"
               >
                 Upload now
               </button>
@@ -96,7 +104,7 @@ export default function ModalUpload() {
                   setModalOpen(false);
                 }}
                 id="cancel"
-                className="ml-3 rounded-sm px-3 py-1 hover:bg-gray-300 focus:shadow-outline focus:outline-none"
+                className="ml-3 rounded-md px-3 py-1 hover:bg-gray-300 focus:shadow-outline focus:outline-none"
               >
                 Cancel
               </button>
--- dasawisma-diskominfo/src/components/TableLaporan.jsx
+++ dasawisma-diskominfo/src/components/TableLaporan.jsx
@@ -2,67 +2,118 @@ import { useState } from "react";
 import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
 import "@cyntler/react-doc-viewer/dist/index.css";
 import ModalUpload from "./ModalUpload";
+import { Link } from "react-router-dom";
 
 export default function TableLaporan() {
   const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const [modalOpen, setModalOpen] = useState(false);
+  const [viewOpen, setViewOpen] = useState(false);
   const [colorrr, setColor] = useState("red");
-  function ViewDoc() {
-    const docs = [
-      {
-        uri: "example.pdf",
-        fileType: "pdf",
-        fileName: "Laporan b",
-      },
-      {
-        uri: "https://s28.q4cdn.com/392171258/files/doc_downloads/test.pdf",
-        fileType: "pdf",
-        fileName: "Laporan A",
-      },
-      {
-        uri: "https://storage.googleapis.com/flip-prod-mktg-strapi/media-library/Indonesian_Foods_80deed788b/Indonesian_Foods_80deed788b.png",
-        fileType: "jpg",
-        fileName: "Laporan A",
-      },
-    ];
-    return (
-      <DocViewer
-        documents={docs}
-        pluginRenderers={DocViewerRenderers}
-        style={{ height: "100vh" }}
-      />
-    );
-  }
+  const handleDelete = () => {
+    return Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success",
+        });
+      }
+    });
+  };
+  // function ViewDoc() {
+  //   const docs = [
+  //     {
+  //       uri: "example.pdf",
+  //       fileType: "pdf",
+  //       fileName: "Laporan b",
+  //     },
+  //     {
+  //       uri: "https://s28.q4cdn.com/392171258/files/doc_downloads/test.pdf",
+  //       fileType: "pdf",
+  //       fileName: "Laporan A",
+  //     },
+  //     {
+  //       uri: "https://storage.googleapis.com/flip-prod-mktg-strapi/media-library/Indonesian_Foods_80deed788b/Indonesian_Foods_80deed788b.png",
+  //       fileType: "jpg",
+  //       fileName: "Laporan A",
+  //     },
+  //   ];
+  //   return (
+  //     <DocViewer
+  //       documents={docs}
+  //       pluginRenderers={DocViewerRenderers}
+  //       style={{ height: "100vh" }}
+  //     />
+  //   );
+  // }
+  const docs = [
+    {
+      uri: "example.pdf",
+      fileType: "pdf",
+      fileName: "Laporan b",
+    },
+    {
+      uri: "https://s28.q4cdn.com/392171258/files/doc_downloads/test.pdf",
+      fileType: "pdf",
+      fileName: "Laporan A",
+    },
+    {
+      uri: "https://storage.googleapis.com/flip-prod-mktg-strapi/media-library/Indonesian_Foods_80deed788b/Indonesian_Foods_80deed788b.png",
+      fileType: "jpg",
+      fileName: "Laporan A",
+    },
+  ];
   const [selectedDocs, setSelectedDocs] = useState([]);
-  const uploadDoc = () => {
+  if (modalOpen) {
+    return <ModalUpload setModalOpen={setModalOpen} />;
+  } else if (viewOpen) {
     return (
       <>
-        <input
-          type="file"
-          accept=".pdf"
-          multiple
-          onChange={(el) =>
-            el.target.files?.length &&
-            setSelectedDocs(Array.from(el.target.files))
-          }
-        />
-        <DocViewer
-          documents={selectedDocs.map((file) => ({
-            uri: window.URL.createObjectURL(file),
-            fileName: file.name,
-          }))}
-          pluginRenderers={DocViewerRenderers}
-        />
+        <div className="flex justify-center items-center h-screen">
+          <div className="max-w-lg w-full">
+            <button
+              onClick={() => setViewOpen(false)}
+              className="bg-slate-400 rounded-md w-14 text-white"
+            >
+              Back
+            </button>
+            <DocViewer
+              documents={docs}
+              pluginRenderers={DocViewerRenderers}
+              style={{ height: "1000" }}
+            />
+          </div>
+        </div>
       </>
     );
-  };
-  if (modalOpen) {
-    return <ModalUpload />;
-  } else if( !modalOpen) {
+  } else if (!modalOpen || !viewOpen) {
     return (
       <>
-        <h6 style={{ color: colorrr }}>iniiiii</h6>
+        {/* <h6 style={{ color: colorrr }}>iniiiii</h6> */}
         <div className="p-6">
+          <Link
+            to={"/dashboard"}
+            className="inline-block p-1 rounded bg-gray-500/10 text-gray-500 font-medium text-[12px] leading-none"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              x="px"
+              y="0px"
+              width="24"
+              height="24"
+              viewBox="0 0 48 48"
+            >
+              <path d="M 24 4 C 12.972066 4 4 12.972074 4 24 C 4 35.027926 12.972066 44 24 44 C 35.027934 44 44 35.027926 44 24 C 44 12.972074 35.027934 4 24 4 z M 24 7 C 33.406615 7 41 14.593391 41 24 C 41 33.406609 33.406615 41 24 41 C 14.593385 41 7 33.406609 7 24 C 7 14.593391 14.593385 7 24 7 z M 22.470703 16.486328 A 1.50015 1.50015 0 0 0 21.439453 16.939453 L 15.439453 22.939453 A 1.50015 1.50015 0 0 0 15.439453 25.060547 L 21.439453 31.060547 A 1.50015 1.50015 0 1 0 23.560547 28.939453 L 20.121094 25.5 L 31.5 25.5 A 1.50015 1.50015 0 1 0 31.5 22.5 L 20.121094 22.5 L 23.560547 19.060547 A 1.50015 1.50015 0 0 0 22.470703 16.486328 z"></path>
+            </svg>
+          </Link>
           <div className="grid grid-cols-1 gap-4 sm:mt-2 sm:grid-cols-2 lg:grid-cols-2 gap-6 mb-2">
             <div className="bg-white border border-gray-200 shadow-md shadow-black/5 p-6 rounded-md lg:col-span-2">
               <div className="flex justify-between mb-4 items-start">
@@ -77,6 +128,9 @@ export default function TableLaporan() {
                       </th>
                       <th className="text-[12px] uppercase tracking-wide font-medium text-gray-400 py-2 px-4 bg-gray-50 text-left">
                         Bulan
+                      </th>
+                      <th className="text-[12px] uppercase tracking-wide font-medium text-gray-400 py-2 px-4 bg-gray-50 text-left rounded-tr-md rounded-br-md">
+                        Kecamatan
                       </th>
                       <th className="text-[12px] uppercase tracking-wide font-medium text-gray-400 py-2 px-4 bg-gray-50 text-left rounded-tr-md rounded-br-md">
                         Action
@@ -101,11 +155,17 @@ export default function TableLaporan() {
                             April
                           </span>
                         </td>
+                        <td className="py-2 px-4 border-b border-b-gray-50">
+                          <span className="text-[13px] font-medium text-gray-500">
+                            Warudoyong
+                          </span>
+                        </td>
                         <td className="flex gap-2 py-2 px-4 border-b border-b-gray-50">
                           <button
                             onClick={() => {
                               // setColor("yellow")
-                              ViewDoc();
+                              // ViewDoc();
+                              setViewOpen(true);
                             }}
                             className="inline-block p-1 rounded bg-emerald-500/10 text-emerald-500 font-medium text-[12px] leading-none"
                           >
@@ -119,9 +179,14 @@ export default function TableLaporan() {
                           >
                             Upload
                           </button>
-                          <span className="inline-block p-1 rounded bg-red-500/10 text-red-500 font-medium text-[12px] leading-none">
+                          <button
+                            onClick={() => {
+                              handleDelete();
+                            }}
+                            className="inline-block p-1 rounded bg-red-500/10 text-red-500 font-medium text-[12px] leading-none"
+                          >
                             Delete
-                          </span>
+                          </button>
                         </td>
                       </tr>
                     ))}
