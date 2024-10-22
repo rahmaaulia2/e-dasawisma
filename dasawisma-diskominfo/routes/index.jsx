46ecc1174cd76c42cdd57d2ce6ad62118ccdcf3c
rahma aulia <qwertyrahmaaulia@gmail.com>
2024-10-22 16:08:02 +0700

    17.2/feat:configure access pages by role

--- dasawisma-diskominfo/routes/index.jsx
+++ dasawisma-diskominfo/routes/index.jsx
@@ -9,9 +9,64 @@ import TableUser from "../src/components/TableUser";
 import TableLaporan from "../src/components/TableLaporan";
 
 const loader = () => {
+  console.log(localStorage.access_token, localStorage.role);
   if (!localStorage.access_token || localStorage.access_token === "undefined") {
     return redirect('/login')
   }
+  return null
+}
+const loaderAdmin = () => {
+  if (localStorage.role !== "admin") {
+    Swal.fire({
+      // title: "Good job!",
+      text: `you're not authorized to access this page`,
+      icon: "info",
+    });
+    return redirect('/')
+  }
+  return null
+}
+const loaderUserAdmin = () => {
+  if (localStorage.role !== "user" || localStorage.role !== "admin") {
+    Swal.fire({
+      title: "Not Authorized",
+      text: `you're not authorized to access this page`,
+      icon: "info",
+    });
+    return redirect('/')
+  }
+  return null
+}
+const loaderSuperUser = () => {
+  if (localStorage.role !== "RT" && localStorage.role !== "RW" && localStorage.role !== "Kecamatan" && localStorage.role !== "Kelurahan" && localStorage.role !== "admin") {
+    Swal.fire({
+      title: "Not Authorized",
+      text: `you're not authorized to access this page`,
+      icon: "info",
+    });
+    return redirect('/')
+  }
+  return null
+}
+const combinedLoaderAdmin = async () => {
+  const accessCheck = loader()
+  if(accessCheck) return accessCheck
+  const adminCheck = loaderAdmin()
+  if(adminCheck) return adminCheck
+  return null
+}
+const combinedLoaderUserAdmin = async () => {
+  const accessCheck = loader()
+  if(accessCheck) return accessCheck
+  const userAdminCheck = loaderUserAdmin()
+  if(userAdminCheck) return userAdminCheck
+  return null
+}
+const combinedLoaderSuperUser = ()=>{
+  const accessCheck = loader()
+  if(accessCheck) return accessCheck
+  const superUserCheck = loaderSuperUser()
+  if(superUserCheck) return superUserCheck
   return null
 }
 
@@ -24,23 +79,23 @@ const router = createBrowserRouter([
     }, {
       path : "/dasawisma",
       element : <Dasawisma/>,
-      loader : loader
+      loader : combinedLoaderUserAdmin
     },{
       path : "/addUser",
       element :<AddUser/>,
-      loader : loader
+      loader : combinedLoaderAdmin
     }, {
       path : "/dashboard",
       element : <Dashboard/>,
-      loader : loader
+      loader : combinedLoaderSuperUser
     }, {
       path : "/users",
       element : <TableUser/>,
-      loader : loader
+      loader : combinedLoaderAdmin
     }, {
       path :"/laporan",
       element :<TableLaporan/>,
-      loader : loader
+      loader : combinedLoaderSuperUser
     }]
   },{
     path : "/login",
